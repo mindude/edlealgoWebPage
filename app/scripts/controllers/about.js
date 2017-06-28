9bb13cb104112b139be86f62fa41b0296891f32f
mindude <edude0j9@gmail.com>
2017-06-27 19:27:43 -0700

    Page commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name edlealgoWpApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the edlealgoWpApp
+ */
+angular.module('edlealgoWpApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name edlealgoWpApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the edlealgoWpApp
+ */
+angular.module('edlealgoWpApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
